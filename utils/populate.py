803797617517bacda95fd9d767d4f06282460e02
Shubham Pandey <shubhampcpandey13@gmail.com>
2021-10-17 05:22:51 +0530

    Add populating script

---
+++ utils/populate.py
@@ -0,0 +1,45 @@
+import sys
+sys.path.append('../')
+
+import os
+import xlrd
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PlantDBBrowser.settings')
+django.setup()
+PATH = os.path.dirname(os.path.realpath(__file__))
+
+from app.models import DataBase, Category
+
+
+def db_excel_parser():
+    loc = (os.path.join(PATH ,"databases.xlsx"))
+    book = xlrd.open_workbook(loc)
+
+
+
+    for index, sheet in enumerate(book.sheets()):
+        if index == 0:
+            continue
+        nrows = sheet.nrows
+        category, _ = Category.objects.get_or_create(
+            name=sheet.name.strip()
+        )
+
+        for row in range(1, nrows):
+            name = sheet.cell(row, 0).value.strip()
+            url = sheet.cell(row, 1).value.strip()
+            desc = sheet.cell(row, 0).value.strip()
+            cit = sheet.cell(row, 0).value.strip()
+
+            DataBase.objects.get_or_create(
+                name=name,
+                url=url,
+                description=desc,
+                citation=cit,
+                category=category
+            )
+
+
+if __name__ == '__main__':
+    db_excel_parser()
