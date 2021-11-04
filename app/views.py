6bc9b6bf71c7d57bef530c5e9b91920e30ddaf91
Shubham Pandey <shubhampcpandey13@gmail.com>
2021-11-04 13:55:59 +0530

    Add table view w/ category filter

--- app/views.py
+++ app/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 
-from .models import DataBase
+from .models import Category, DataBase
 
 
 class DatabaseListView(ListView):
     model = DataBase
     context_object_name = "databases"
-    queryset = DataBase.objects.filter(approved=True)
 
+    def get_queryset(self):
+        category_name = self.request.GET.get("category", None)
+        if category_name is not None:
+            category = Category.objects.filter(name=category_name).first()
+        else:
+            category = Category.objects.first()
+        return DataBase.objects.filter(
+            approved=True,
+            category=category
+        )
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["categories"] = list(Category.objects.values_list("name", flat=True))
+        return context
