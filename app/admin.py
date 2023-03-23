e434bb7cc0e194bf5672eede15c358caf87b0834
Shubham Pandey <shubhampcpandey13@gmail.com>
2023-03-23 11:59:14 +0530

    update gene admin interface

--- app/admin.py
+++ app/admin.py
@@ -3,15 +3,15 @@ from django.contrib import admin
 from .models import Category, DataBase, SubCategory, Gene, Species, GeneSuggestion
 
 
-@admin.register(DataBase)
-class DataBaseAdmin(admin.ModelAdmin):
-    list_display = ["name", "url", "category", "sub_category"]
-    list_filter = ["approved", "category","sub_category"]
+@admin.register(Gene)
+class GeneAdmin(admin.ModelAdmin):
+    list_display = ["name", "species", "approved"]
+    list_filter = ["approved", "species","function", "experimental_method"]
     search_fields = ["name"]
 
 
-admin.site.register(Category)
-admin.site.register(SubCategory)
-admin.site.register(Gene)
-admin.site.register(Species)
-admin.site.register(GeneSuggestion)
+# admin.site.register(Category)
+# admin.site.register(SubCategory)
+# admin.site.register(Gene)
+# admin.site.register(Species)
+# admin.site.register(GeneSuggestion)
